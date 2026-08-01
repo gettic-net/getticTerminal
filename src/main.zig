a03c2638e5f99193ff0061c19a44d914e0926ef6
gettic <hasanseyh6a@gmail.com>
2026-08-01 19:54:16 +0300

    Create main.zig

---
+++ src/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const cli = @import("cli.zig");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    try cli.start(allocator);
+}
